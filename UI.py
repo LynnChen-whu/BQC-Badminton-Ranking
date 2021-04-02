ef7fcd40bd09fb6cd067d3e1ba384d69fd316557
LynnChen <lynnchen0014@outlook.com>
2021-04-03 02:14:53 +0800

    Add files via upload

--- UI.py
+++ UI.py
@@ -16,10 +16,15 @@ def list_is_dig_list(list_in: list[str]) -> bool:
             return False
     return True
 
-
 def list_is_key_list(list_in: list, dic_in: dict) -> bool:
     for key in list_in:
         if not key in list(dic_in.keys()):
+            return False
+    return True
+
+def list_is_name_list(list_in: list[str], Ranking_System: Elo_Module.Ranking_System) -> bool:
+    for name in list_in:
+        if Ranking_System.inquire_by_name(name) == '没有找到该选手':
             return False
     return True
 
@@ -43,38 +48,62 @@ if __name__ == '__main__':
         player_list.append(Elo_Module.Player(player_data_temp[0], int(player_data_temp[1])))
     ranking_system = Elo_Module.Ranking_System(system_name, player_list)
 
+    #ranking_system.reset_rating()
+
     while 1:
 
         print(f'当前{system_name}情况如下')
         ranking_list = list(ranking_system.get_rankings().keys())
         ranking_list.sort()
         for ranking in ranking_list:
-            print(str(ranking).rjust(3) + '\t' + ranking_system.get_rankings()[ranking].get_name().ljust(7) +
+            print(str(ranking).rjust(3) + '\t' + ranking_system.get_rankings()[ranking].get_name().ljust(9) +
                   '\t' + str(ranking_system.get_rankings()[ranking].get_rating()).rjust(5))
 
         flag = easygui.buttonbox('请选择你要进行的操作', system_name, ['录入比赛结果', '新增选手', '退出系统'])
 
         if flag == '录入比赛结果':
 
-            team1 = easygui.enterbox('请输入胜方当前的排名，不同选手间用小数点“.”隔开', flag, ).split('.')
-            while not list_is_dig_list(team1):
-                team1 = easygui.enterbox('输入非法，请重新输入胜方当前的排名，不同选手间用小数点“.”隔开', flag).split('.')
-            for index in range(len(team1)):
-                team1[index] = int(team1[index])
-            while not list_is_key_list(team1, ranking_system.get_rankings()):
-                team1 = easygui.enterbox('输入非法，请重新输入胜方当前的排名，不同选手间用小数点“.”隔开', flag).split('.')
-            team1 = ranking_system.get_player_list_by_ranking_list(team1)
+            team1 = easygui.enterbox('请输入胜方选手的姓名，不同选手间用中文逗号“，”隔开', flag)
+            check = 0
+            if team1 is not None:
+                team1 = team1.split('，')
+                if list_is_name_list(team1, ranking_system):
+                    check = 1
+                while check == 0:
+                    team1 = easygui.enterbox('输入非法，请重新输入胜方选手的姓名，不同选手间用中文逗号“，”隔开', flag)
+                    if team1 is not None:
+                        team1 = team1.split('，')
+                        if list_is_name_list(team1, ranking_system):
+                            check = 1
+                    else:
+                        break
+                if check == 0:
+                    continue
+            else:
+                continue
+            team1 = ranking_system.get_player_list_by_name_list(team1)
 
-            team0 = easygui.enterbox('请输入败方当前的排名，不同选手间用小数点“.”隔开', flag).split('.')
-            while len(team0) != len(team1):
-                team0 = easygui.enterbox('输入非法，请重新输入败方当前的排名，不同选手间用小数点“.”隔开', flag).split('.')
-            while not list_is_dig_list(team0):
-                team0 = easygui.enterbox('输入非法，请重新输入败方当前的排名，不同选手间用小数点“.”隔开', flag).split('.')
-            for index in range(len(team0)):
-                team0[index] = int(team0[index])
-            while not list_is_key_list(team0, ranking_system.get_rankings()):
-                team0 = easygui.enterbox('输入非法，请重新输入败方当前的排名，不同选手间用小数点“.”隔开', flag).split('.')
-            team0 = ranking_system.get_player_list_by_ranking_list(team0)
+            team0 = easygui.enterbox('请输入败方选手的姓名，不同选手间用中文逗号“，”隔开', flag)
+            check = 0
+            if team0 is not None:
+                team0 = team0.split('，')
+                if len(team0) == len(team1):
+                    if list_is_name_list(team0, ranking_system):
+                        check = 1
+                while check == 0:
+                    team0 = easygui.enterbox('输入非法，请重新输入败方选手的姓名，不同选手间用中文逗号“，”隔开', flag)
+                    if team0 is not None:
+                        team0 = team0.split('，')
+                        if len(team0) == len(team1):
+                            if list_is_name_list(team0, ranking_system):
+                                check = 1
+                    else:
+                        break
+                if check == 0:
+                    continue
+            else:
+                continue
+            team0 = ranking_system.get_player_list_by_name_list(team0)
 
             ranking_system.match(team0, team1, 0, 17)
 
